9fbfda971362cc353aea162b4de9a94c4533e814
steven-yanh <hyan0820@gmail.com>
2022-09-30 00:37:02 -0400

    DemoImages Update

--- Bank-IOS/Bank-IOS/Model/ProfileManager.swift
+++ Bank-IOS/Bank-IOS/Model/ProfileManager.swift
@@ -8,19 +8,19 @@
 import Foundation
 struct ProfileManager {
     func fetchProfile(forUserid userId: String,  completion: @escaping (Result<Profile,NetworkError>) -> Void) {
-        let url = URL(string: "https://fierce-retreat-36855.herokuapp.com/bankey/profile/\(userId)")!
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data, error == nil else {
-                completion(.failure(.serverError))
-                return
-            }
-            do {
-                let profile = try JSONDecoder().decode(Profile.self, from: data)
-                completion(.success(profile))
-            } catch {
+//        let url = URL(string: "https://fierce-retreat-36855.herokuapp.com/bankey/profile/\(userId)")!
+//
+//        URLSession.shared.dataTask(with: url) { data, response, error in
+//            guard let data = data, error == nil else {
+//                completion(.failure(.serverError))
+//                return
+//            }
+//            do {
+//                let profile = try JSONDecoder().decode(Profile.self, from: data)
+//                completion(.success(profile))
+//            } catch {
                 completion(.failure(.decodeError))
-            }
-        }.resume()
+//            }
+//        }.resume()
     }
 }
